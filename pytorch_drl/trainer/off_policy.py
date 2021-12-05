83c9f28ff224891bcf55147d7710611a7aca58e5
yizhang <yizhang.dev@gmail.com>
2021-12-05 22:14:15 +0100

    update trainer

--- pytorch_drl/trainer/off_policy.py
+++ pytorch_drl/trainer/off_policy.py
@@ -24,7 +24,7 @@ class OffPolicyTrainer(BaseTrainer):
         )
         self.logger = EpisodeLogger()
 
-    def __call__(self, mode):
+    def __call__(self, reward_func):
         # set the agent in training mode
         self.agent.train()
         for episode in range(self.num_episodes):
@@ -37,7 +37,7 @@ class OffPolicyTrainer(BaseTrainer):
                 # select an action
                 action = self.agent.act(state)
                 # perform the action and observe new state
-                next_state, reward, done, info = self.env.step(action, mode=mode)
+                next_state, reward, done, info = self.env.step(action, reward_func=reward_func)
                 # buffer the experience
                 self.agent.cache(state, action, reward, done, next_state)
                 # learn from the experience
